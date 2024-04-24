2529ca911b4d1632919ba4674e253cdc9d892b34
Denis Pozder <denispozder6@gmail.comm>
2024-04-24 23:18:30 +0200

    Saving Changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,11 @@ import Hero from "./Components/Hero/Hero";
 import RecentProjects from "./Components/RecentProjects/RecentProjects";
 import AboutSection from "./Components/AboutSection/AboutSection";
 
+export const metadata = {
+  title: "Wort",
+  description: "Welcome to Wort, where we specialize in conceptualizing and designing engaging websites and brand identities.",
+};
+
 export default function Home() {
   return (
     <main className="page_section">
